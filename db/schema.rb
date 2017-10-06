e0eb12893f7c072b49b179ed167b84a3667bd980
Ella Nicolson <nicolson@grinnell.edu>
2017-10-06 23:26:45 +0000

    Added somes definitions to user controller; added phone to user schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171006182241) do
+ActiveRecord::Schema.define(version: 20171006232346) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "phone"
   end
 
 end
