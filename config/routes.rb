1c3dc1feab4428fee6424eb84ac3b631e00ae61c
Ella Nicolson <nicolson@grinnell.edu>
2017-10-07 00:03:53 +0000

    Changed routes to incorporate interests

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  resources :interests
+  
+  resources :users do
+    resources :interests
+  end
+  
   get  '/signup',  to: 'users#new'
-
-
-  resources :users
+    
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171006232346) do
+ActiveRecord::Schema.define(version: 20171006234409) do
+
+  create_table "interests", force: :cascade do |t|
+    t.string "name"
+    t.string "user"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
